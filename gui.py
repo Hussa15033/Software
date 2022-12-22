8b5033312d694e15947a3c9e046cd09e056edd8b
Hussain, Auwais (k19028326) <auwais.hussain@kcl.ac.uk>
2022-12-22 20:13:55 +0000

    Add configuration modal to GUI

--- gui.py
+++ gui.py
@@ -3,9 +3,58 @@ import tkinter as tk
 root = tk.Tk()
 root.geometry("800x500")
 root.title("Population Protocol")
+
+class ConfigurationModal:
+	def __init__(self, parent):
+		# Setup validation variable -> Modal is not validated intially
+		self.validated = False
+
+		self.top = tk.Toplevel(parent)
+		self.top.grab_set()
+		tk.Label(self.top, text='Number of nodes:').pack()
+		self.number_nodes_entry = tk.Entry(self.top)
+		self.number_nodes_entry.pack()
+
+		tk.Label(self.top, text='Max number of states:').pack()
+		self.number_states_entry = tk.Entry(self.top)
+		self.number_states_entry.pack()
+
+		tk.Label(self.top, text='Number of faulty nodes:').pack()
+		self.number_faulty_nodes_entry = tk.Entry(self.top)
+		self.number_faulty_nodes_entry.pack()
+
+		tk.Label(self.top, text='Max number of rounds (optional):').pack()
+		self.number_rounds_entry = tk.Entry(self.top)
+		self.number_rounds_entry.pack()
+
+		tk.Label(self.top, text='Protocol:').pack()
+
+		# Protocol selection menu
+		protocol_list = ["voter", "2-choice", "3-majority"]
+
+		chosen_protocol = tk.StringVar(self.top)
+		chosen_protocol.set("Select protocol")
+
+		w = tk.OptionMenu(self.top, chosen_protocol, *protocol_list)
+		w.pack()
+
+		tk.Button(self.top, text='Simulate!', command=self.is_valid).pack()
+
+	def is_valid(self):
+		# Check all inputs are valid, if so destroy modal
+		# otherwise show an alert for the relevant input
+		self.top.destroy()
+
+
+def show_config_modal():
+	configModal = ConfigurationModal(root)
+	root.wait_window(configModal.top)
+	print("Finished.")
+
 # Top bar
 top_bar = tk.Frame(root, bg="blue")
-create_protocol = tk.Button(top_bar, text = "Create protocol..")
+
+create_protocol = tk.Button(top_bar, text = "Create protocol..", command = show_config_modal)
 
 # Graph panel
 graph = tk.Frame(root, bg="#ff0000")
