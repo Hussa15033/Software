b99f439d3c4ab25aa71fc6b9dd18985cd94d4a2a
Hussain, Auwais (k19028326) <auwais.hussain@kcl.ac.uk>
2023-02-04 21:59:45 +0000

    Fix bug with nogui option in parser

--- parser.py
+++ parser.py
@@ -45,7 +45,7 @@ PROTOCOLS = {"threemajority": ThreeMajority}
 parser = argparse.ArgumentParser(prog = 'Population Protocol Visualiser',
 								description = 'Analyse and visualise population protocol networks')
 
-parser.add_argument('-nogui', '--nogui', action = 'store_true', dest = 'nogui', help='Run the analyser without a GUI', default=True)
+parser.add_argument('-nogui', '--nogui', action = 'store_true', dest = 'nogui', help='Run the analyser without a GUI', default=False)
 parser.add_argument('-r', '-rounds', dest = 'rounds', help = 'The number of rounds to run the protocol for, only valid when used with -nogui', type = positive_number, default=DEFAULT_ROUNDS_COUNT)
 parser.add_argument('-n', '-nodes', dest = 'nodes', help = 'The number of nodes in the network', type=positive_number, default=DEFAULT_NODE_COUNT)
 parser.add_argument('-s', '-states', dest = 'states', help = 'The number of initial states, or a list of comma separated numbers indicating how many nodes in each state (must add up to n, the number of nodes)', type=network_config, default=[DEFAULT_STATE_COUNT])
@@ -73,9 +73,13 @@ else:
 
 network = PopulationNetwork(args.nodes, network_states, protocol, state_config)
 
-SimulationGUI(network)
+print(args.nogui)
+if args.nogui:
+	print("Running with no gui..?")
+else:
+	SimulationGUI(network)
 
-for i in range(100000000):
-	network.run_round()
+# for i in range(100000000):
+	# network.run_round()
 print("Done")
 # if (args.)
